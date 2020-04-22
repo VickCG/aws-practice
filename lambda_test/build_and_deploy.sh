7d73e7d531cbd9e33c7c5d0d980f06958a8ab4dc
vicknguyen1992 <vietnguyen11101992@gmail.com>
2020-04-22 16:53:10 +0700

    Update build and deploy bash

--- lambda_test/build_and_deploy.sh
+++ lambda_test/build_and_deploy.sh
@@ -1,3 +1,3 @@
 sam build;
 sam package --output-template packaged.yaml --s3-bucket merl-workhorse-lambda-functions;
-sam deploy --template-file packaged.yaml --region us-east-2 --capabilities CAPABILITY_IAM --stack-name sam-get-status-stack;
+sam deploy --template-file packaged.yaml --capabilities CAPABILITY_IAM --stack-name sam-get-status-stack;
